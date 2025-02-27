58ee2af3b60f4fc9e568c81701fd8c2a7af97d77
dod06be <katynskii.kiu@phystech.edu>
2025-02-27 15:44:06 +0300

    ooops

--- 27.02/task4.cpp
+++ 27.02/task4.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 #include <vector>
 
-double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
-    if (nums1.size()<nums2.size()) return findMedianSortedArrays(nums2, nums1);
-    int lo=0, hi=nums2.size()*2;
-    while(lo<=hi){
-        int mid2=(lo+hi)/2;
-        int mid1=nums1.size()+nums2.size()-mid2;
-        double L1=(mid1==0) ? INT_MIN : nums1[(mid1-1)/2];
-        double L2=(mid2==0) ? INT_MIN : nums2[(mid2-1)/2];
-        double R1=(mid1==nums1.size()*2) ? INT_MAX : nums1[(mid1)/2];
-        double R2=(mid2==nums2.size()*2) ? INT_MAX : nums2[(mid2)/2];
-        
-        if(L1>R2) lo=mid2+1;
-        else if(L2>R1) hi=mid2-1;
-        else return (std::max(L1,L2) + std::min(R1, R2))/2;
+int greater_or_equal(std::vector<int>& A, int B){
+    int n=A.size();
+    if(n==0||A[n-1]<B) return n;
+    int lp{0}, rp{1};
+    while(rp<n&&A[rp]<B){
+        lp=rp;
+        rp*=2;
     }
-    return -1;
-} 
+    rp=std::min(n-1, rp);
+    while(lp<=rp){
+        int mp=(lp+rp)/2;
+        if(A[mp]>=B) rp=mp-1;
+        else lp=mp+1;
+    }
+    return lp;
+}
+
+int main(){
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<int> A(n), B(m);
+    for(int i{0}; i<n; ++i) std::cin >> A[i];
+    for(int i{0}; i<m; ++i) std::cin >> B[i];
+    for(auto i : B) std::cout << greater_or_equal(A, i) << " ";
+    std::cout << std::endl;
+}
